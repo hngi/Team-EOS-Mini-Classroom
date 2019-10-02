ec96cd65c9493c62264d766b966b8436933ecf0a
Jaylukmann <lafbnyunus@yahoo.com>
2019-10-03 00:53:40 +0100

    student-login.php

--- student-login.php
+++ student-login.php
@@ -1,4 +1,4 @@
-<?php 
+   <?php 
   include 'inc/student-header.php';
 ?>
 
@@ -19,34 +19,32 @@
           ?>
             <h1>Log In As Student</h1>
             <form action="" method="post">
-                <div class="text-box">
-                    <input class="email" type="email" name="email" placeholder="Email Address">
-                </div>
-                <div class="text-box">
-                    <input type="password" class="Password" name="password" placeholder="password">
-                </div>
-                <input type="submit" class="button" name="submit" value="Sign In" style="color: #fff;">
-                <input type="checkbox" class="checkbox" required style="margin-right: 0;">  <span style="margin: 0; padding: 0;">Remember Password</span>
-            </form>
-
-            
-            <br>
-            <div class="footer">
-                <p1>Don't have an account ?<a href="student_sign_up.php" style="color: #274970; text-decoration: none; font-weight: bold;"> Sign Up</a></p1>
-            </div>
+              <div class="text-box">
+                  <input class="email" name="email" value="" placeholder="Email Address">
+              </div>
+              <div class="text-box">
+                  <input class="Password" name="password" value="" placeholder="Password">
+              </div>
+              <!-- Remember Password -->
+              <input type="checkbox" class="checkbox" required>  <span style="">Remember Password</span>
+              <button type="submit" name="submit" class="button" style="color: #fff;">Sign In</button>
+          </form>
+    
+           <br>
+          <div class="footer">
+            <p>Don't have an account ?<a href="teacher_sign_up.php" style="color: #274970; text-decoration: none; font-weight: bold;"> Sign Up</a></p><br>
+            <p><a href="reset.php" style="color: #274970; text-decoration: none; font-weight: bold;">Forgotten Password</a></p>
+          </div>        
         </div>
         <div class="picture-cont" >
             <img src="images/teacher.png" class="picture">
         </div>
       </div>
-    </div>
-  </div>
-    <div class="col-md-4">
+    
 
     </div>
-    <img src="images/Ellipse2.png" class="Ellipse2">
-    <img src="images/x.png" class="x">
+    
 
-<script src="js/main.js"></script>
+  <script src="js/main.js"></script>
 </body>
 </html>
