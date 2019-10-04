99efb190eba231c1a8910b3d4e0b3e5b99d76733
emex4gman <emex4gman@gmail.com>
2019-10-04 18:26:26 +0100

    cool nav bar

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@ function classToggle() {
 
 document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
+
+
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+var toggleBtn = document.querySelector('.Navbar__Link-toggle')
+// On click
+var nav = document.querySelector('.nav')
+toggleBtn.addEventListener("click", function () {
+    nav.classList.toggle("open");
+
+});
+
+toggleButton.addEventListener("click", function () {
+    // Toggle class "is-active"
+    // hamburger.classList.toggle("is-active");
+    // Do something else, like open/close menu
+    mobileNav.classList.toggle("open");
+
+});
