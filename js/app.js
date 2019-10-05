604651c98429b19bdc5e9ffccabc49b12e7a7228
emex4gman <emex4gman@gmail.com>
2019-10-05 13:54:45 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+alert('collented ')
+
 function classToggle() {
     const navs = document.querySelectorAll('.nav__links')
 
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
 }
-
+ 
 document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
 
@@ -18,10 +20,30 @@ toggleBtn.addEventListener("click", function () {
 
 });
 
-toggleButton.addEventListener("click", function () {
-    // Toggle class "is-active"
-    // hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-    mobileNav.classList.toggle("open");
+// toggleButton.addEventListener("click", function () {
+//     // Toggle class "is-active"
+//     // hamburger.classList.toggle("is-active");
+//     // Do something else, like open/close menu
+//     mobileNav.classList.toggle("open");
 
-});
+// });
+
+
+// const toggle = document.getElementById('#toggle');
+// const body = document.getElementById('body');
+
+// toggle.addEventListener('click', e => {
+//     const isChecked = e.target.checked;
+
+//     if (isChecked) {
+//         body.classList.add('dark-theme');
+//     } else {
+//         body.classList.remove('dark-theme');
+//     }
+// });
+
+// toggle.addEventListener('click', () => {
+//     const isChecked = e.target.checked;
+// body.classList.toggle('dark-theme')
+// });
+
